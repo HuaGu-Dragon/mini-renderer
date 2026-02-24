8f92895d52ce8472037787b708cfa6a17525f177
俺不叫化咕龙 <1801943622@qq.com>
2026-02-24 18:38:25 +0800

    Update rasterizer.rs

--- src/graphics/rasterizer.rs
+++ src/graphics/rasterizer.rs
@@ -7,6 +7,7 @@ use crate::{
 pub struct Fragment<V> {
     pub x: usize,
     pub y: usize,
+    pub depth: f32,
     pub varying: V,
 }
 
@@ -29,14 +30,15 @@ impl TriangleRasterizer {
         Self { width, height }
     }
 
-    fn clip_to_screen(&self, clip_pos: Vec4) -> Vec2 {
+    fn clip_to_screen(&self, clip_pos: Vec4) -> Vec4 {
         let ndc_x = clip_pos.x / clip_pos.w;
         let ndc_y = clip_pos.y / clip_pos.w;
+        let ndc_z = clip_pos.z / clip_pos.w;
 
         let screen_x = (ndc_x + 1.) * 0.5 * self.width as f32;
         let screen_y = (1. - ndc_y) * 0.5 * self.height as f32;
 
-        Vec2::new(screen_x, screen_y)
+        Vec4::new(screen_x, screen_y, ndc_z, clip_pos.w)
     }
 
     fn edge_function(a: Vec2, b: Vec2, c: Vec2) -> f32 {
@@ -45,9 +47,9 @@ impl TriangleRasterizer {
 
     fn rasterize_triangle<Var>(
         &self,
-        v0: Vec2,
-        v1: Vec2,
-        v2: Vec2,
+        v0: Vec4,
+        v1: Vec4,
+        v2: Vec4,
         v0_varying: &Var,
         v1_varying: &Var,
         v2_varying: &Var,
@@ -67,7 +69,11 @@ impl TriangleRasterizer {
         let min_y = min_y.max(0);
         let max_y = max_y.min(self.height as i32);
 
-        let area = Self::edge_function(v0, v1, v2);
+        let area = Self::edge_function(
+            Vec2::new(v0.x, v0.y),
+            Vec2::new(v1.x, v1.y),
+            Vec2::new(v2.x, v2.y),
+        );
 
         if area <= 0.0 {
             return fragments;
@@ -77,21 +83,35 @@ impl TriangleRasterizer {
             for x in min_x..max_x {
                 let p = Vec2::new(x as f32 + 0.5, y as f32 + 0.5);
 
-                let w0 = Self::edge_function(v1, v2, p);
-                let w1 = Self::edge_function(v2, v0, p);
-                let w2 = Self::edge_function(v0, v1, p);
+                let w0 = Self::edge_function(Vec2::new(v1.x, v1.y), Vec2::new(v2.x, v2.y), p);
+                let w1 = Self::edge_function(Vec2::new(v2.x, v2.y), Vec2::new(v0.x, v0.y), p);
+                let w2 = Self::edge_function(Vec2::new(v0.x, v0.y), Vec2::new(v1.x, v1.y), p);
+
+                let weight0 = (w0 / area) / v0.w;
+                let weight1 = (w1 / area) / v1.w;
+                let weight2 = (w2 / area) / v2.w;
+
+                let sum = weight0 + weight1 + weight2;
 
                 if w0 >= 0.0 && w1 >= 0.0 && w2 >= 0.0 {
                     fragments.push(Fragment {
                         x: x as usize,
                         y: y as usize,
+                        depth: Varying::interpolate(
+                            &v0.z,
+                            &v1.z,
+                            &v2.z,
+                            weight0 / sum,
+                            weight1 / sum,
+                            weight2 / sum,
+                        ),
                         varying: Varying::interpolate(
                             v0_varying,
                             v1_varying,
                             v2_varying,
-                            w0 / area,
-                            w1 / area,
-                            w2 / area,
+                            w0 / area / v0.w,
+                            w1 / area / v1.w,
+                            w2 / area / v2.w,
                         ),
                     });
                 }
