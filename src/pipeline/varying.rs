166eef27dca785fc70ae8b2a7f56fb008de08797
俺不叫化咕龙 <1801943622@qq.com>
2026-03-08 16:23:33 +0800

    Update varying.rs

--- src/pipeline/varying.rs
+++ src/pipeline/varying.rs
@@ -16,6 +16,17 @@ impl<T> Varying for PhantomData<T> {
 }
 
 macro_rules! impl_varying {
+    ([$($T:ident $idx:tt),*]) => {
+        impl<$($T: Varying,)*> Varying for ($($T,)*) {
+            fn interpolate(v0: Self, v1: Self, v2: Self, w0: f32, w1: f32, w2: f32) -> Self {
+                (
+                    $(
+                        Varying::interpolate(v0.$idx, v1.$idx, v2.$idx, w0, w1, w2),
+                    )*
+                )
+            }
+        }
+    };
     ($($ty:ty),*) => {
         $(
             impl Varying for $ty {
@@ -28,22 +39,16 @@ macro_rules! impl_varying {
 }
 
 impl_varying!(f32, f64);
-
-impl Varying for (f32, f32) {
-    fn interpolate(v0: Self, v1: Self, v2: Self, w0: f32, w1: f32, w2: f32) -> Self {
-        (
-            Varying::interpolate(v0.0, v1.0, v2.0, w0, w1, w2),
-            Varying::interpolate(v0.1, v1.1, v2.1, w0, w1, w2),
-        )
-    }
-}
-
-impl Varying for (f32, f32, f32) {
-    fn interpolate(v0: Self, v1: Self, v2: Self, w0: f32, w1: f32, w2: f32) -> Self {
-        (
-            Varying::interpolate(v0.0, v1.0, v2.0, w0, w1, w2),
-            Varying::interpolate(v0.1, v1.1, v2.1, w0, w1, w2),
-            Varying::interpolate(v0.2, v1.2, v2.2, w0, w1, w2),
-        )
-    }
-}
+impl_varying!([T0 0]);
+impl_varying!([T0 0, T1 1]);
+impl_varying!([T0 0, T1 1, T2 2]);
+impl_varying!([T0 0, T1 1, T2 2, T3 3]);
+impl_varying!([T0 0, T1 1, T2 2, T3 3, T4 4]);
+impl_varying!([T0 0, T1 1, T2 2, T3 3, T4 4, T5 5]);
+impl_varying!([T0 0, T1 1, T2 2, T3 3, T4 4, T5 5, T6 6]);
+impl_varying!([T0 0, T1 1, T2 2, T3 3, T4 4, T5 5, T6 6, T7 7]);
+impl_varying!([T0 0, T1 1, T2 2, T3 3, T4 4, T5 5, T6 6, T7 7, T8 8]);
+impl_varying!([T0 0, T1 1, T2 2, T3 3, T4 4, T5 5, T6 6, T7 7, T8 8, T9 9]);
+impl_varying!([T0 0, T1 1, T2 2, T3 3, T4 4, T5 5, T6 6, T7 7, T8 8, T9 9, T10 10]);
+impl_varying!([T0 0, T1 1, T2 2, T3 3, T4 4, T5 5, T6 6, T7 7, T8 8, T9 9, T10 10, T11 11]);
+impl_varying!([T0 0, T1 1, T2 2, T3 3, T4 4, T5 5, T6 6, T7 7, T8 8, T9 9, T10 10, T11 11, T12 12]);
