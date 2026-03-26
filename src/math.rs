972d45ee06729ece9f51f277e72df6ef0cd8ffab
俺不叫化咕龙 <1801943622@qq.com>
2026-03-26 22:19:43 +0800

    feat: own math type supported Add own math types when glam disabled

--- src/math.rs
+++ src/math.rs
@@ -1,10 +1,56 @@
 #[cfg(feature = "glam")]
-pub type Vec2 = glam::Vec2;
+pub use glam::*;
+
+#[cfg(not(feature = "glam"))]
+pub use inner::*;
+
 #[cfg(feature = "glam")]
-pub type Vec3 = glam::Vec3;
-#[cfg(feature = "glam")]
-pub type Vec4 = glam::Vec4;
-#[cfg(feature = "glam")]
-pub type Mat3 = glam::Mat3;
-#[cfg(feature = "glam")]
-pub type Mat4 = glam::Mat4;
+mod glam {
+    pub type Vec2 = glam::Vec2;
+    pub type Vec3 = glam::Vec3;
+    pub type Vec4 = glam::Vec4;
+    pub type Mat3 = glam::Mat3;
+    pub type Mat4 = glam::Mat4;
+}
+
+#[cfg(not(feature = "glam"))]
+mod inner {
+    #[derive(Debug, Default, Clone, Copy)]
+    pub struct Vec2 {
+        pub x: f32,
+        pub y: f32,
+    }
+
+    impl Vec2 {
+        pub fn new(x: f32, y: f32) -> Self {
+            Self { x, y }
+        }
+    }
+
+    #[derive(Debug, Default, Clone, Copy)]
+    pub struct Vec3 {
+        pub x: f32,
+        pub y: f32,
+        pub z: f32,
+    }
+
+    impl Vec3 {
+        pub fn new(x: f32, y: f32, z: f32) -> Self {
+            Self { x, y, z }
+        }
+    }
+
+    #[derive(Debug, Default, Clone, Copy)]
+    pub struct Vec4 {
+        pub x: f32,
+        pub y: f32,
+        pub z: f32,
+        pub w: f32,
+    }
+
+    impl Vec4 {
+        pub fn new(x: f32, y: f32, z: f32, w: f32) -> Self {
+            Self { x, y, z, w }
+        }
+    }
+}
