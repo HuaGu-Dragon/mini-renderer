8d61d65d835676208383730558119a49b5e39d02
俺不叫化咕龙 <1801943622@qq.com>
2026-03-28 21:08:58 +0800

    Update graphics.rs

--- src/graphics.rs
+++ src/graphics.rs
@@ -2,11 +2,13 @@ pub mod primitive;
 pub mod rasterizer;
 pub mod topology;
 
+#[derive(Debug, PartialEq)]
 pub enum FrontFace {
     Ccw,
     Cw,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum Face {
     Front,
     Back,
