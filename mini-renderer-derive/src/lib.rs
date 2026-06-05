32ddf93d17d4998c6e045675684e42411062f187
俺不叫化咕龙 <1801943622@qq.com>
2026-06-05 20:46:20 +0800

    Support derive `Varying` for TupleStruct

--- mini-renderer-derive/src/lib.rs
+++ mini-renderer-derive/src/lib.rs
@@ -1,27 +1,43 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{DeriveInput, parse_macro_input};
+use syn::{DeriveInput, parse_macro_input, parse_quote};
 
 #[proc_macro_derive(Varying)]
 pub fn derive_varying(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
     let ident = input.ident;
-    let syn::Data::Struct(data) = input.data else {
-        panic!("Only support struce yet")
+    let generics = add_trait_bounds(input.generics);
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let varying = {
+        match input.data {
+            syn::Data::Struct(ref data) => {
+                let fields = &data.fields;
+                fields.iter().enumerate().map(|(n,f)| {
+                if let Some(ident )= &f.ident {
+                    quote! {
+                        #ident: ::mini_renderer::pipeline::varying::Varying::interpolate(v0.#ident, v1.#ident, v2.#ident, w0, w1, w2)
+                    }
+                } else {
+                    let n = syn::Index::from(n);
+                    quote! {
+                        #n: ::mini_renderer::pipeline::varying::Varying::interpolate(v0.#n, v1.#n, v2.#n, w0, w1, w2)
+                    }
+                }
+            })
+            }
+            syn::Data::Enum(ref _data) => {
+                todo!()
+            }
+            syn::Data::Union(ref _data) => {
+                todo!()
+            }
+        }
     };
 
-    let fields = data.fields;
-
-    let varying = fields.iter().map(|f| {
-        let ident = &f.ident;
-        quote! {
-            #ident: ::mini_renderer::pipeline::varying::Varying::interpolate(v0.#ident, v1.#ident, v2.#ident, w0, w1, w2)
-        }
-    });
-
     quote! {
-        impl ::mini_renderer::pipeline::varying::Varying for #ident {
+        impl #impl_generics ::mini_renderer::pipeline::varying::Varying for #ident #ty_generics #where_clause {
             fn interpolate(v0: Self, v1: Self, v2: Self, w0: f32, w1: f32, w2: f32) -> Self {
                 Self {
                     #(#varying),*
@@ -30,4 +46,15 @@ pub fn derive_varying(input: TokenStream) -> TokenStream {
         }
     }
     .into()
+}
+
+fn add_trait_bounds(mut generics: syn::Generics) -> syn::Generics {
+    for param in &mut generics.params {
+        if let syn::GenericParam::Type(ref mut type_param) = *param {
+            type_param
+                .bounds
+                .push(parse_quote!(::mini_renderer::pipeline::varying::Varying));
+        }
+    }
+    generics
 }
---
+++ tests/pass/derive_tuple_test.rs
@@ -0,0 +1,6 @@
+use mini_renderer::Varying;
+
+#[derive(Clone, Copy, Varying)]
+pub struct VaryingTest((f32, f32), (f32, f32, f32));
+
+fn main() {}
